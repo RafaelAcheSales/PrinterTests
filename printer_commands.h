51459ee10731b2457c205b114ebc0cb643174b3c
RafaelAcheSales <rafael.ache@hotmail.com>
2021-06-23 09:18:01 -0300

    fisrt commit

---
+++ printer_commands.h
@@ -0,0 +1,8 @@
+char *TEST_01 = "\x1b\x4d\x01 Fonte01 \n"
+                "\x1b\x4d\x02 Fonte02 \n"
+                "\x1b\x4d\x03 Fonte03 \n"
+                "\x1b\x4d\x04 Fonte04 \n";
+
+const char *PRINT_LINE_FEED = "\x0a";
+const char *PRINT_CARRIAGE_RETURN = "\x0d";
+
