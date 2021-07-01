11deb4503c4ce7f209b03ad3a21bf0cb2acad35d
RafaelAcheSales <rafael.ache@hotmail.com>
2021-07-01 17:49:22 -0300

    fixed menu

--- printertester.c
+++ printertester.c
@@ -8,6 +8,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include "printer_commands.h"
+#include "bmp.h"
 
 int set_interface_attribs(int fd, int speed)
 {
@@ -63,50 +64,10 @@ void set_mincount(int fd, int mcount)
 
 int main()
 {
+    
     char *portname = TERMINAL;
     int fd;
     int wlen;
-
-    char command[1024] = "\x1d\x28\x6b";
-    char input[512];
-    scanf("%s",input);
-    printf("%s\n",input);
-    int input_size = strlen(input);
-    int size = input_size + 3;
-    command[3] = size % 256;
-    command[4] = size / 256;
-    command[5] = 49;
-    command[6] = 80;
-    command[7] = 48;
-
-
-    int actual_size = 8+input_size;
-    //strncat(command, teste, len);
-    for (int i = 8; i < actual_size; i++)
-    {
-        command[i] = input[i-8];
-    }
-
-    command[actual_size] = 29;
-    command[actual_size+1] = 40;
-    command[actual_size+2] = 107;
-    command[actual_size+3] = 3;
-    command[actual_size+4] = 0;
-    command[actual_size+5] = 49;
-    command[actual_size+6] = 81;
-    command[actual_size+7] = 48;
-
-
-    actual_size += 8;
-    for (int i = 0; i < actual_size; i++)
-    {
-        printf("%x, %c\n", command[i], command[i]);
-    }
-
-
-    int command_lenght = actual_size;
-
-
 
     fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
     if (fd < 0) {
@@ -116,25 +77,157 @@ int main()
     /*baudrate 115200, 8 bits, no parity, 1 stop bit */
     set_interface_attribs(fd, B9600);
     //set_mincount(fd, 0);                /* set to pure timed read */
-
+    int command_size = 0;
+    
     /* simple output */
+    while (1)
+    {
+        command_size = 0;
+        //printf("enviando comando: %s \n", command);
+        int input;
+        int maxOptions = 3;
+        printf("Selecione a operaçao\n");
+        printf("1 - impressao de texto\n");
+        printf("2 - impressao de QRcode\n");
+        printf("3 - cortar papel\n");
+        input = getchar();
+        input = input - 48;
+        printf("input is %d\n", input);
+        fflush(stdin);
+        switch (input)
+        {
+        case 1:
+            command_size = strlen(TEST_02);
+            wlen = write(fd, TEST_02, command_size);
+            if (wlen != command_size) {
+                printf("Error from write: %d, %d\n", wlen, errno);
+            }
+            tcdrain(fd);
+            break;
+        case 2:
+            //
+            printf("Escreva o valor do qrcode: \n");
+            char qr_value[256];
+            scanf("%s", qr_value);
+            int input_size = strlen(qr_value);
+            int size = input_size + 3;
 
-    //printf("enviando comando: %s \n", command);
-    char init_qr_cmd[16] = "\x1D\x28\x6B\x03\x00\x31\x43\x01\x1D\x28\x6B\x03\x00\x31\x45\x33";
-    printf("Digite o tamanho 0-16: ");
-    int sizeInput;
-    scanf("%d", &sizeInput);
-    init_qr_cmd[7] = sizeInput;
-    //wlen = write(fd, init_qr_cmd, 16);
-    //wlen = write(fd, command, command_lenght);
-    //wlen = write(fd, PARTIAL_CUT_ONE, 7);
-    wlen = write(fd, LOGO_IMG, 5000);
+            char command[1024] = "\x1d\x28\x6b";
+            command[3] = size % 256;
+            command[4] = size / 256;
+            command[5] = 49;
+            command[6] = 80;
+            command[7] = 48;
 
-    printf("%d \n", wlen);
-    if (wlen != command_lenght) {
-        printf("Error from write: %d, %d\n", wlen, errno);
+
+            int actual_size = 8+input_size;
+            //strncat(command, teste, len);
+            for (int i = 8; i < actual_size; i++)
+            {
+                command[i] = qr_value[i-8];
+            }
+
+            command[actual_size] = 29;
+            command[actual_size+1] = 40;
+            command[actual_size+2] = 107;
+            command[actual_size+3] = 3;
+            command[actual_size+4] = 0;
+            command[actual_size+5] = 49;
+            command[actual_size+6] = 81;
+            command[actual_size+7] = 48;
+
+
+            actual_size += 8;
+            for (int i = 0; i < actual_size; i++)
+            {
+                printf("%x, %c\n", command[i], command[i]);
+            }
+
+
+            int command_lenght = actual_size;
+
+
+
+            printf("Digite o tamanho 0-16: ");
+            int sizeInput;
+            scanf("%d", &sizeInput);
+            char *init_qr = INIT_QRCODE;
+            init_qr[7] = sizeInput;
+            wlen = write(fd, init_qr, 16);
+            wlen = write(fd, command, command_lenght);
+            //wlen = write(fd, PARTIAL_CUT_ONE, 7);
+            //wlen = write(fd, LOGO_IMG, 5000);
+            break;
+        case 3:
+            //
+            command_size = strlen(PARTIAL_CUT_ONE);
+            wlen = write(fd, PARTIAL_CUT_ONE, command_size);
+            if (wlen != command_size) {
+                printf("Error from write: %d, %d\n", wlen, errno);
+            }
+            tcdrain(fd);
+            break;
+        
+        default:
+            printf("Not reckognized\n\n");
+            break;
+        }
+        fflush(stdin);
     }
-    tcdrain(fd);    /* delay for output */
+
+    //     int input_size = strlen(input);
+    //     int size = input_size + 3;
+
+    //     char command[1024] = "\x1d\x28\x6b";
+    //     command[3] = size % 256;
+    //     command[4] = size / 256;
+    //     command[5] = 49;
+    //     command[6] = 80;
+    //     command[7] = 48;
+
+
+    //     int actual_size = 8+input_size;
+    //     //strncat(command, teste, len);
+    //     for (int i = 8; i < actual_size; i++)
+    //     {
+    //         command[i] = input[i-8];
+    //     }
+
+    //     command[actual_size] = 29;
+    //     command[actual_size+1] = 40;
+    //     command[actual_size+2] = 107;
+    //     command[actual_size+3] = 3;
+    //     command[actual_size+4] = 0;
+    //     command[actual_size+5] = 49;
+    //     command[actual_size+6] = 81;
+    //     command[actual_size+7] = 48;
+
+    
+    //     actual_size += 8;
+    //     for (int i = 0; i < actual_size; i++)
+    //     {
+    //         printf("%x, %c\n", command[i], command[i]);
+    //     }
+
+
+    //     int command_lenght = actual_size;
+
+
+
+    //     printf("Digite o tamanho 0-16: ");
+    //     int sizeInput;
+    //     scanf("%d", &sizeInput);
+    //     char *
+    //     init_qr_cmd[7] = sizeInput;
+    //     //wlen = write(fd, init_qr_cmd, 16);
+    //     //wlen = write(fd, command, command_lenght);
+    //     //wlen = write(fd, PARTIAL_CUT_ONE, 7);
+    //     //wlen = write(fd, LOGO_IMG, 5000);
+
+    //     printf("%d \n", wlen);
+            /* delay for output */
+    // }
+    
 
     //wlen = write(fd, command2, command2_lenght);
 
